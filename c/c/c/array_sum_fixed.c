11f0d7354b1c29bdf2840c874144891a60e86d25
Mutitujohn871 <kimanijohnt7@gmail.com>
2026-01-28 14:01:02 +0300

    Create array_sum_fixed.c

---
+++ c/c/c/array_sum_fixed.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int sum(int arr[], int n) {
+    int total = 0;
+    for(int i = 0; i < n; i++) { // Fixed
+        total += arr[i];
+    }
+    return total;
+}
+
+int main() {
+    int nums[] = {1, 2, 3};
+    printf("Sum: %d\n", sum(nums, 3));
+    return 0;
+}
